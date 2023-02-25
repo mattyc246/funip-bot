760d0bfd49a1b4a8fcaeeff0b8236fc86c03d8f4
Matthew Cross <mattyc246@gmail.com>
2023-02-25 13:37:19 +0800

    effective hp calculation command

--- commands/doubleSummons.js
+++ commands/doubleSummons.js
@@ -1,12 +1,25 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, formatEmoji } = require('discord.js');
+
+const getEmojiId = (type) => {
+  switch (type) {
+    case 'ancient':
+      return '1078894401838907442';
+    case 'void':
+      return '1078894411066388532';
+    case 'sacred':
+      return '1078894406893043762';
+  }
+};
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('2x')
     .setDescription('Check the next upcoming 2x event!'),
   async execute(interaction) {
+    const shard = formatEmoji(getEmojiId('ancient'));
+
     await interaction.reply(`
-      **UPCOMING 2x EVENT**\n---------------\nAncient shards from 24th February to 27th February\n\nExtra Harima 10x event running along side the 2x
+      **UPCOMING 2x EVENT**\n------------------\n${shard} Ancient shards from 24th February to 27th February\n\nExtra Harima 10x event running along side the 2x
     `);
   }
 };
---
+++ commands/effectiveHP.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, parseEmoji } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ehp')
+    .setDescription('Calculate the effective HP from HP and DEF')
+    .addStringOption((option) =>
+      option.setName('hp').setDescription('Champions HP').setRequired(true)
+    )
+    .addStringOption((option) =>
+      option.setName('def').setDescription('Champions DEF').setRequired(true)
+    ),
+  async execute(interaction) {
+    const defense = interaction.options.getString('def');
+    const hp = interaction.options.getString('hp');
+
+    let parsedDef;
+    let parsedHp;
+
+    try {
+      parsedDef = parseInt(defense);
+      parsedHp = parseInt(hp);
+
+      if (isNaN(parsedDef) || isNaN(parsedHp)) {
+        throw new Error('Provided values are not a number!');
+      }
+    } catch (error) {
+      return await interaction.reply(error.message);
+    }
+
+    const calculatedEHP = Math.floor(
+      parsedHp / (1 - 0.85 * (1 - 2.718 ** ((-2 * parsedDef) / 3000)))
+    );
+
+    await interaction.reply(
+      `**CALCULATED EHP**\n---------------------\nYour effective HP is:\n**${calculatedEHP}**`
+    );
+  }
+};
