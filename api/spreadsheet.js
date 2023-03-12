486168f5221e7a0209169348ada1d25f3e894c89
Matthew Cross <mattyc246@gmail.com>
2023-03-12 15:55:22 +0800

    use strapi to get spreadsheet url

---
+++ api/spreadsheet.js
@@ -0,0 +1,28 @@
+const {
+  apiClient,
+  failedResponse,
+  HTTP_STATUS_OK,
+  successResponse
+} = require('./axios');
+
+const getSpreadsheetUrl = async () => {
+  try {
+    const url = `/api/spreadsheet-url`;
+    const response = await apiClient.get(url);
+
+    console.log(response?.data?.data?.attributes);
+
+    if (response?.status === HTTP_STATUS_OK) {
+      return successResponse({
+        data: response?.data?.data?.attributes?.url
+      });
+    } else {
+      return failedResponse();
+    }
+  } catch (error) {
+    console.log('Error: ', error);
+    return failedResponse();
+  }
+};
+
+module.exports = { getSpreadsheetUrl };
