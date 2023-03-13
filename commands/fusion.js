994e889563c22e9673235dfc650ab02ae1148eed
Matthew Cross <mattyc246@gmail.com>
2023-03-13 17:24:02 +0800

    update fusion embed

--- commands/fusion.js
+++ commands/fusion.js
@@ -2,6 +2,21 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { getFusionData } = require('../api/fusion');
 const moment = require('moment');
 
+const getAffinityColor = (affinity) => {
+  switch (affinity) {
+    case 'Void':
+      return 0xcb3dc9;
+    case 'Spirit':
+      return 0x52c548;
+    case 'Force':
+      return 0xe54b3c;
+    case 'Magic':
+      return 0x59c0f9;
+    default:
+      return 0x000000;
+  }
+};
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('fusion')
@@ -21,13 +36,46 @@ module.exports = {
       const startDate = moment(activeFusion?.start_date).format('Do MMMM YYYY');
       const endDate = moment(activeFusion?.end_date).format('Do MMMM YYYY');
 
-      const embed = new EmbedBuilder().setImage(imageUrl);
+      const embedObject = {
+        color: getAffinityColor(activeFusion?.affinity),
+        title: activeFusion?.champion_name,
+        description: 'Current Raid Shadow Legends Champion Fusion',
+        fields: [
+          {
+            name: `Start Date`,
+            value: startDate,
+            inline: true
+          },
+          {
+            name: `End Date`,
+            value: endDate,
+            inline: true
+          },
+          {
+            name: `Fusion Type`,
+            value: activeFusion?.fusion_type
+          },
+          {
+            name: 'Faction',
+            value: activeFusion?.faction,
+            inline: true
+          },
+          {
+            name: `Affinity`,
+            value: activeFusion?.affinity,
+            inline: true
+          }
+        ],
+        image: {
+          url: imageUrl
+        }
+      };
 
-      const message = `**FUSION DETAILS**\n----------------\n**Champion:** ${activeFusion?.champion_name}\n**Affinity:** ${activeFusion?.affinity}\n**Faction:** ${activeFusion?.faction}\n**Fusion Type:** ${activeFusion?.fusion_type}\n**From:** ${startDate}\n**Until:** ${endDate}`;
+      const message = 'Thanks nerd!';
 
-      await interaction.reply(message);
+      await interaction.reply({ content: message, ephemeral: true });
       return await interaction.channel.send({
-        embeds: [embed]
+        embeds: [embedObject]
       });
     } else {
       return await interaction.reply('BOT ERROR!');
