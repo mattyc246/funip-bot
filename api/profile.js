ec366b5b8409e7ff648e351a48cf129ddd191e90
Matthew Cross <mattyc246@gmail.com>
2023-03-20 22:54:31 +0800

    added optimizer commands

---
+++ api/profile.js
@@ -0,0 +1,43 @@
+const {
+  failedResponse,
+  apiClient,
+  HTTP_STATUS_OK,
+  successResponse
+} = require('./axios');
+
+const createProfile = async (data) => {
+  if (!data) return failedResponse();
+  try {
+    const url = `/api/profiles`;
+    const response = await apiClient.post(url, data);
+
+    if (response?.status === HTTP_STATUS_OK) {
+      return successResponse();
+    } else {
+      return failedResponse();
+    }
+  } catch (error) {
+    return failedResponse({
+      error: error.response.data.error.name
+    });
+  }
+};
+
+const getProfiles = async () => {
+  try {
+    const url = `/api/profiles`;
+    const response = await apiClient.get(url);
+
+    if (response?.status === HTTP_STATUS_OK) {
+      return successResponse({
+        profiles: response?.data?.data
+      });
+    } else {
+      return failedResponse();
+    }
+  } catch (error) {
+    return failedResponse();
+  }
+};
+
+module.exports = { createProfile, getProfiles };
