64c1fd6a142c0e91e0e971ddbe5b253d0979f3c2
Matthew Cross <mattyc246@gmail.com>
2023-04-16 01:23:19 +0800

    use cloudinary image upload

--- api/imageUploader.js
+++ api/imageUploader.js
@@ -1,21 +1,20 @@
-import {
-  getDownloadURL,
-  ref,
-  uploadBytes,
-  uploadString
-} from 'firebase/storage';
+import cloudinary from 'cloudinary';
 import { failedResponse, successResponse } from './axios.js';
-import { storage } from '../services/firebase.js';
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 export const uploadImageFile = async (dataUrl, filename) => {
   if (!dataUrl || !filename) return failedResponse();
   try {
-    const storageRef = ref(storage, `funip-images/${filename}`);
+    const response = await cloudinary.v2.uploader.upload(dataUrl, {
+      public_id: filename.split('.')[0]
+    });
 
-    // 'file' comes from the Blob or File API
-    await uploadString(storageRef, dataUrl, 'data_url');
-
-    const url = await getDownloadURL(storageRef);
+    const url = response.secure_url;
 
     return successResponse({
       data: url
