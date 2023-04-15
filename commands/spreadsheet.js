2c888514e0511cca60de27394527f25b849b65d1
Matthew Cross <mattyc246@gmail.com>
2023-04-15 12:07:10 +0800

    hard code spreadsheet

--- commands/spreadsheet.js
+++ commands/spreadsheet.js
@@ -1,38 +1,32 @@
 import { SlashCommandBuilder } from 'discord.js';
-import { getSpreadsheetUrl } from '../api/spreadsheet.js';
 
 export const data = new SlashCommandBuilder()
   .setName('spreadsheet')
   .setDescription('Get the URL to the clan spreadsheet');
 
 export async function execute(interaction) {
-  const response = await getSpreadsheetUrl();
-
-  if (response?.isSuccess) {
-    const embedObject = {
-      color: 0x37d44f,
-      title: 'Karens Spreadsheet',
-      description: 'Check out Karens nerdery!',
-      fields: [
-        {
-          name: 'Clan Info',
-          value: 'Information about the clans'
-        },
-        {
-          name: 'Click to view',
-          value: response?.data
-        }
-      ],
-      thumbnail: {
-        url: 'https://res.cloudinary.com/dejxmm6g7/image/upload/v1678602502/Ice_Fu_4d670431b7.png?updated_at=2023-03-12T06:28:43.411Z'
+  const embedObject = {
+    color: 0x37d44f,
+    title: 'Karens Spreadsheet',
+    description: 'Check out Karens nerdery!',
+    fields: [
+      {
+        name: 'Clan Info',
+        value: 'Information about the clans'
+      },
+      {
+        name: 'Click to view',
+        value:
+          'https://docs.google.com/spreadsheets/d/1aBVkLPr9sfAo2v9JalhxyPu20ogdjUsPBEVEtvYNVDQ'
       }
-    };
+    ],
+    thumbnail: {
+      url: 'https://res.cloudinary.com/dejxmm6g7/image/upload/v1678602502/Ice_Fu_4d670431b7.png?updated_at=2023-03-12T06:28:43.411Z'
+    }
+  };
 
-    await interaction.reply({ content: `Thanks nerd!`, ephemeral: true });
-    return await interaction.channel.send({
-      embeds: [embedObject]
-    });
-  } else {
-    await interaction.reply('BOT ERROR!');
-  }
+  await interaction.reply({ content: `Thanks nerd!`, ephemeral: true });
+  return await interaction.channel.send({
+    embeds: [embedObject]
+  });
 }
