5fb07a3c028c635810af93e1efbaa8c6bf84687d
Matthew Cross <mattyc246@gmail.com>
2023-09-11 14:10:28 +0800

    Doom tower boss command

---
+++ data/doomTowerBossData.js
@@ -0,0 +1,50 @@
+export const bosses = [
+  {
+    id: 1,
+    bossName: 'Frost Spider',
+    threadId: '1112536307646865448',
+    color: 0x4fcdff
+  },
+  {
+    id: 2,
+    bossName: 'Eternal Dragon',
+    threadId: '1112535450117214238',
+    color: 0x6fff4f
+  },
+  {
+    id: 3,
+    bossName: 'Nether Spider',
+    threadId: '1112516093156794478',
+    color: 0xfa0707
+  },
+  {
+    id: 4,
+    bossName: 'Scarab King',
+    threadId: '1112514243720392777',
+    color: 0xebd575
+  },
+  {
+    id: 5,
+    bossName: 'Bommal the Dreadhorn',
+    threadId: '1112511075443814492',
+    color: 0xf58002
+  },
+  {
+    id: 6,
+    bossName: 'Dark Fae',
+    threadId: '1112509758067458080',
+    color: 0x9c02f5
+  },
+  {
+    id: 7,
+    bossName: 'Magma Dragon',
+    threadId: '1112508031230545920',
+    color: 0x1b2736
+  },
+  {
+    id: 8,
+    bossName: 'Celestial Griffin',
+    threadId: '1112506207077417092',
+    color: 0x9e6700
+  }
+];
